09993df65cb8ec8e4e586aae220a192d51cb2554
YHaJung <49065638+YHaJung@users.noreply.github.com>
2020-11-24 13:10:56 +0900

    mypage main만듬 , lecture card 깨짐 해결

--- frontend/src/app/mypage/mypage.component.ts
+++ frontend/src/app/mypage/mypage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService} from '../api.service';
 
 @Component({
   selector: 'app-mypage',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MypageComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private apiService: ApiService) { }
+  profile:any=[];//name, nickname, email, phonenumber   -- 필수
+  personalInfo:any=[];//school, job, level, birthday, gender, category[], subcategory[]    -- 선택
   ngOnInit(): void {
+    this.apiService.getPersonalInfo().subscribe(
+      data => {
+        this.personalInfo = data['result'];
+        console.log(this.personalInfo);
+      },
+      error => console.log(error)
+    );
+    this.apiService.getProfile().subscribe(
+      data => {
+        this.profile = data['result'];
+        console.log(this.profile);
+      },
+      error => console.log(error)
+    );
   }
 
 }
