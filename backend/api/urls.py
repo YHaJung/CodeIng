1c94d50c805f88a02673f7a7a049c2af97b41e23
meongeun <bluechip17@naver.com>
2021-01-26 13:54:47 +0900

    1

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,27 +6,21 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-# router.register('movies', MovieViewSet)
-# router.register('reviews', ReviewViewSet)
-# router.register('users', UserViewSet)
-
 
 urlpatterns = [
-    # path('review/', views.review_list, name='review_list'),
-    # path('', views.movie_list, name='movie_list'),
     path('', include(router.urls)),
-    # url(r'^api/tutorials$', views.tutorial_list),
-    # url(r'^api/tutorials/(?P<pk>[0-9]+)$', views.tutorial_detail),
-    # <int:pk>
-    url(r'^api/KNN_IBCF/(?P<pk>\d+)$', views.KNN_IBCF),
-    url(r'^api/KNN_UBCF/(?P<pk>\d+)$', views.KNN_UBCF),
+    url(r'^api/create_model/$', views.create_model),
     url(r'^api/recommend_save/$', views.recommend_save),
-    # url(r'^api/(?P<pk>\d+)$', views.CBRS),
+
+    url(r'^api/knn_ibcf/(?P<pk>\d+)$', views.KNN_IBCF),
+    url(r'^api/knn_ubcf/(?P<pk>\d+)$', views.KNN_UBCF),
+
+    # 쓰이는 api
     path('api/user_recommend', views.CBRS),
     path('api/user_recommendlist', views.CBRSlist),
     url(r'^api/recommend/$', views.Poprs),
     url(r'^api/recommendlist', views.Poprslist),
-    url(r'^api/CREATE_MODEL/$', views.create_model),
+
     url(r'^api/matrixfactorization_model', views.create_matrixFactorization_IBCF),
     path('api/<int:pk>/matrixfactorization_IBCF', views.sim_movies_to),
     path('api/<int:pk>/matrixfactorization_UBCF', views.recommend_movies_to),
